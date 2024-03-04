8858cc3473fefeff8ae78259b81a20794d6efc67
jteleha <telehajoel@gmail.com>
2024-03-04 11:35:10 -0500

    Added license link function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -10,7 +10,13 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license) {
+    return `This project is licensed under the [${license}](https://opensource.org/licenses/${license}) license.`;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
